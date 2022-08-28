648fc486b3c9fed26fe8f8096a69cd70ae844048
Meng Demao <mengdemao19951021@163.com>
2022-08-28 14:10:51 +0800

    * fix cross compiler build error

--- build.sh
+++ build.sh
@@ -1,49 +1,32 @@
 #!/bin/bash
 
+# 设置
 root_path=$(pwd)
-
 target=arm-linux-gnueabihf
+tmp_path=${root_path}/object
 
-object_dir=${root_path}/object
+export INSTALL_PATH=${root_path}/${target}
 
-target_dir=${root_path}/${target}
+version_gcc=12.2.0
+version_binutil=2.39
+version_glic=2.36
+version_linux=4.19.229
 
-export PATH=${target_dir}/bin:$PATH
+dir_gcc=gcc-${version_gcc}
+dir_linux=linux-${version_linux}
+dir_glibc=glibc-${version_glic}
+dir_binutils=binutils-${version_binutil}
 
-dir_gmp=gmp-6.2.1
-file_gmp=${dir_gmp}.tar.xz
-
-dir_mpc=mpc-1.2.1
-file_mpc=${dir_mpc}.tar.gz
-
-dir_mpfr=mpfr-4.1.0
-file_mpfr=${dir_mpfr}.tar.xz
-
-dir_isl=isl-0.24
-file_isl=${dir_isl}.tar.xz
-
-dir_cloog=cloog-0.18.1
-file_cloog=${dir_cloog}.tar.gz
-
-dir_gcc=gcc-7.5.0
-file_gcc=${dir_gcc}.tar.xz
-
-dir_binutils=binutils-2.38
 file_binutils=${dir_binutils}.tar.xz
-
-dir_linux=linux-4.19.229
+file_gcc=${dir_gcc}.tar.xz
 file_linux=${dir_linux}.tar.xz
-
-dir_glibc=glibc-2.35
 file_glibc=${dir_glibc}.tar.xz
 
 # 创建临时文件夹
-prepare_resource() {
-
-    if [ -d "${object_dir}" ]; then
-        rm -rf "${object_dir}"
-    fi
-    mkdir -p "${object_dir}"
+prepare_resource() 
+{
+    [ -d "${tmp_path}" ] && rm -rf  "${tmp_path}"
+    mkdir -p "${tmp_path}"
 
     if [ ! -f "${file_binutils}" ]; then
         wget https://mirrors.tuna.tsinghua.edu.cn/gnu/binutils/${file_binutils}
@@ -59,98 +42,56 @@ prepare_resource() {
     fi
 
     # 创建目标文件夹
-    if [ -d "${target_dir}" ]; then
-        rm -rf "${target_dir}"
-    fi
-    mkdir -p "${target_dir}"
-
+    [ -d "${INSTALL_PATH}" ] && rm -rf  "${INSTALL_PATH}"
+    mkdir -p "${INSTALL_PATH}"
+    
     # 解压binutils
-    echo -e "start uncompress ${file_binutils} to ${object_dir}\n"
-    tar -vxf ${file_binutils} -C "${object_dir}"
-    echo -e "end uncompress ${file_binutils} to ${object_dir}\n"
+    echo -e "start uncompress ${file_binutils} to ${tmp_path}\n"
+    tar -vxf ${file_binutils} -C "${tmp_path}"
+    echo -e "end uncompress ${file_binutils} to ${tmp_path}\n"
 
     # 解压gcc
-    echo -e "start uncompress ${file_gcc} to ${object_dir}\n"
-    tar -vxf ${file_gcc} -C "${object_dir}"
-    echo -e "end uncompress ${file_gcc} to ${object_dir}\n"
+    echo -e "start uncompress ${file_gcc} to ${tmp_path}\n"
+    tar -vxf ${file_gcc} -C "${tmp_path}"
 
-    # 解压gmp
-    echo -e "start uncompress ${file_gmp} to ${object_dir}\n"
-    tar -vxf ${file_gmp} -C "${object_dir}"
-    echo -e "end uncompress ${file_gmp} to ${object_dir}\n"
+    # 执行代码依赖下载    
+    cd "${tmp_path}"/${dir_gcc} || exit
+    ./contrib/download_prerequisites
+    cd - || exit
 
-    # 解压mpc
-    echo -e "start uncompress ${file_mpc} to ${object_dir}\n"
-    tar -vxf ${file_mpc} -C "${object_dir}"
-    echo -e "end uncompress ${file_mpc} to ${object_dir}\n"
-
-    # 解压mpfr
-    echo -e "start uncompress ${file_mpfr} to ${object_dir}\n"
-    tar -vxf ${file_mpfr} -C "${object_dir}"
-    echo -e "end uncompress ${file_mpfr} to ${object_dir}\n"
-
-    # 解压isl
-    echo -e "start uncompress ${file_isl} to ${object_dir}\n"
-    tar -vxf ${file_isl} -C "${object_dir}"
-    echo -e "end uncompress ${file_isl} to ${object_dir}\n"
-
-    # 解压isl
-    echo -e "start uncompress ${file_cloog} to ${object_dir}\n"
-    tar -vxf ${file_cloog} -C "${object_dir}"
-    echo -e "end uncompress ${file_cloog} to ${object_dir}\n"
-
-    # 解压mpfr
-    echo -e "start uncompress ${file_cloog} to ${object_dir}\n"
-    tar -vxf ${file_cloog} -C "${object_dir}"
-    echo -e "end uncompress ${file_cloog} to ${object_dir}\n"
-
-    # 创建链接
-    ln -s "${object_dir}"/${dir_isl} "${object_dir}"/${dir_gcc}/isl
-    ln -s "${object_dir}"/${dir_gmp} "${object_dir}"/${dir_gcc}/gmp
-    ln -s "${object_dir}"/${dir_mpc} "${object_dir}"/${dir_gcc}/mpc
-    ln -s "${object_dir}"/${dir_mpfr} "${object_dir}"/${dir_gcc}/mpfr
-    ln -s "${object_dir}"/${dir_cloog} "${object_dir}"/${dir_gcc}/cloog
+    echo -e "end uncompress ${file_gcc} to ${tmp_path}\n"
 
     # 解压头文件
-    echo -e "start uncompress ${file_linux} to ${object_dir}\n"
-    tar -vxf ${file_linux} -C "${object_dir}"
-    echo -e "end uncompress ${file_linux} to ${object_dir}\n"
+    echo -e "start uncompress ${file_linux} to ${tmp_path}\n"
+    tar -vxf ${file_linux} -C "${tmp_path}"
+    echo -e "end uncompress ${file_linux} to ${tmp_path}\n"
 
     # 解压glibc
-    echo -e "start uncompress ${file_glibc} to ${object_dir}\n"
-    tar -vxf ${file_glibc} -C "${object_dir}"
-    echo -e "end uncompress ${file_glibc} to ${object_dir}\n"
+    echo -e "start uncompress ${file_glibc} to ${tmp_path}\n"
+    tar -vxf ${file_glibc} -C "${tmp_path}"
+    echo -e "end uncompress ${file_glibc} to ${tmp_path}\n"
 }
 
-# 编译内核头文件
-prepare_kernel_header() {
-    echo -e "start compile linux kernel header"
-    pushd "${object_dir}"/${dir_linux} >>/dev/null || exit
-    make ARCH=arm INSTALL_HDR_PATH="${target_dir}"/${target} headers_install || exit
-    popd >>/dev/null || exit
-    echo -e "end compile linux kernel header"
-}
-
-# 编译binutils第一遍
+# 编译binutils
 build_binutils() {
     echo -e "start compile binutils"
-    pushd "${object_dir}"/${dir_binutils} >>/dev/null || exit
+    pushd "${tmp_path}"/${dir_binutils} >>/dev/null || exit
+    [ -d build ] && rm -rf build 
     mkdir build
     pushd build >>/dev/null || exit
 
     ../configure \
-        CFLAGS="-flto -flto-compression-level=10 -O2 -pipe -ffunction-sections -fdata-sections" \
-        CXXFLAGS="-flto -flto-compression-level=10 -O2 -pipe -ffunction-sections -fdata-sections" \
-        --prefix="${target_dir}" \
+        --prefix="${INSTALL_PATH}" \
         --target=${target} \
+        --with-sysroot="${INSTALL_PATH}" \
+        --with-lib-path="${INSTALL_PATH}"/lib \
         --disable-werror \
         --enable-lto \
         --disable-gdb \
         --disable-nls \
         --enable-gold \
         --enable-plugins \
-        --enable-relro \
-        --with-sysroot || exit
+        --enable-relro || exit
 
     make -j "$(nproc)" || exit
     make install || exit
@@ -160,30 +101,41 @@ build_binutils() {
     echo -e "end compile binutils"
 }
 
+# 编译内核头文件
+build_kernel_header() 
+{
+    echo -e "start compile linux kernel header"
+    pushd "${tmp_path}"/${dir_linux} >>/dev/null || exit
+    make ARCH=arm INSTALL_HDR_PATH="${INSTALL_PATH}"/${target} headers_install || exit
+    popd >>/dev/null || exit
+    echo -e "end compile linux kernel header"
+}
+
 # 第一次编译gcc
-build_gcc_first() {
+build_gcc_first() 
+{
     echo -e "start compile gcc first step"
-    pushd "${object_dir}"/${dir_gcc} >>/dev/null || exit
+    pushd "${tmp_path}"/${dir_gcc} >>/dev/null || exit
+
+    [ -d build ] && rm -rf build 
     mkdir build
     pushd build >>/dev/null || exit
     ../configure \
-        CFLAGS="-flto -flto-compression-level=10 -O2 -pipe -ffunction-sections -fdata-sections" \
-        CXXFLAGS="-flto -flto-compression-level=10 -O2 -pipe -ffunction-sections -fdata-sections" \
         --target=${target} \
-        --prefix="${target_dir}" \
-        --with-glibc-version=2.35 \
+        --prefix="${INSTALL_PATH}" \
+        --with-glibc-version=2.36 \
+        --enable-bootstrap \
+        --enable-threads=posix \
+        --enable-check=release \
         --enable-languages=c,c++ \
         --disable-multilib \
         --with-mode=arm \
-        --with-float-hard ||
+        --with-float=hard ||
         exit
 
     make all-gcc -j "$(nproc)" || exit
     make install-gcc -j "$(nproc)" || exit
-    
-    make all-target-libgcc
-    make install-target-libgcc
-    
+
     popd >>/dev/null || exit
     popd >>/dev/null || exit
     echo -e "end compile gcc first step"
@@ -192,22 +144,28 @@ build_gcc_first() {
 # 第一次编译glibc
 build_glibc_first() {
     echo -e "start compile glibc first step"
-    pushd "${object_dir}"/${dir_glibc} >>/dev/null || exit
+    pushd "${tmp_path}"/${dir_glibc} >>/dev/null || exit
+    
+    [ -d build ] && rm -rf build 
     mkdir build
     pushd build >>/dev/null || exit
     ../configure \
-        --prefix="${target_dir}" \
-        --build="${MACHTYPE}" \
+        --prefix="${INSTALL_PATH}/${target}" \
         --host=${target} \
+        --target=${target} \
         --build="$(../scripts/config.guess)" \
         --enable-kernel=3.2 \
-        --with-headers="${target_dir}"/${target}/include \
+        --with-headers="${INSTALL_PATH}"/${target}/include \
         --disable-multilib \
-        libc_cv_forced_unwind=yes \
+        libc_cv_forced_unwind=yes                     \
+        libc_cv_ctors_header=yes                      \
+        libc_cv_c_cleanup=yes                         \
         with_selinux=no || exit
-    make -j "$(nproc)" || exit
     make install-bootstrap-headers=yes install-headers || exit
-
+    make -j"$(nproc)" csu/subdir_lib 
+    install csu/crt1.o csu/crti.o csu/crtn.o "${INSTALL_PATH}"/${target}/lib
+    ${target}-gcc -nostdlib -nostartfiles -shared -x c /dev/null -o "${INSTALL_PATH}"/${target}/lib/libc.so
+    touch "${INSTALL_PATH}"/${target}/include/gnu/stubs.h
     popd >>/dev/null || exit
     popd >>/dev/null || exit
     echo -e "end compile glibc first step"
@@ -216,7 +174,7 @@ build_glibc_first() {
 # 第二次编译gcc
 build_gcc_second() {
     echo -e "start compile gcc second step"
-    pushd "${object_dir}"/${dir_gcc}/build >>/dev/null || exit
+    pushd "${tmp_path}"/${dir_gcc}/build >>/dev/null || exit
     make all-target-libgcc -j "$(nproc)" || exit
     make install-target-libgcc -j "$(nproc)" || exit
     popd >>/dev/null || exit
@@ -226,7 +184,7 @@ build_gcc_second() {
 # 第二次编译glibc
 build_glibc_second() {
     echo -e "start compile glibc second step"
-    pushd "${object_dir}"/${dir_glibc}/build >>/dev/null || exit
+    pushd "${tmp_path}"/${dir_glibc}/build >>/dev/null || exit
     make -j "$(nproc)" || exit
     make install -j "$(nproc)" || exit
     popd >>/dev/null || exit
@@ -236,7 +194,7 @@ build_glibc_second() {
 # 第三次编译gcc
 build_gcc_third() {
     echo -e "start compile gcc third step"
-    pushd "${object_dir}"/${dir_gcc}/build >>/dev/null || exit
+    pushd "${tmp_path}"/${dir_gcc}/build >>/dev/null || exit
     make -j "$(nproc)" || exit
     make install -j "$(nproc)" || exit
     popd >>/dev/null || exit
@@ -245,7 +203,7 @@ build_gcc_third() {
 
 build_kernel() {
     echo -e "start test compile"
-    pushd "${object_dir}"/${dir_gcc} >>/dev/null || exit
+    pushd "${tmp_path}"/${dir_linux} >>/dev/null || exit
     make ARCH=arm CROSS_COMPILE=${target}- distclean || exit
     make ARCH=arm CROSS_COMPILE=${target}- imx_v6_v7_defconfig || exit
     make ARCH=arm CROSS_COMPILE=${target}- -j "$(nproc)" || exit
@@ -253,9 +211,9 @@ build_kernel() {
     echo -e "end test compile"
 }
 
-prepare_resource
-prepare_kernel_header
+# prepare_resource
 build_binutils
+build_kernel_header
 build_gcc_first
 build_glibc_first
 build_gcc_second
